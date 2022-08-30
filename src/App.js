2da3029a2c6c9a0289cd0dad2a168de115da0f9d
Martin Olague <molague23@hotmail.com>
2022-08-29 22:23:56 -0700

    Added temporary ReminderModal as a template for Add/edit function.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import TodoListForm from './components/TodoListForm';
 import TodoListItems from './components/TodoListItems';
 import Header from './components/Header';
+import { useState } from 'react';
+import CreateReminderModal from './shared/CreateReminderModal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <TodoListForm />
-        <TodoListItems />
+    <>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <TodoListForm setIsOpen={setIsOpen} />
+          <TodoListItems />
+        </div>
+        {isOpen && <CreateReminderModal setIsOpen={setIsOpen} />}
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/TodoListForm.js
+++ src/components/TodoListForm.js
@@ -1,12 +1,18 @@
-function TodoListForm() {
+import { useState } from 'react';
+import CreateReminderModal from '../shared/CreateReminderModal';
+
+function TodoListForm({ setIsOpen }) {
+  const handleCreateReminder = (e) => {
+    e.preventDefault();
+    setIsOpen(true);
+  };
   return (
     <div className="title">
       <form>
         <div className="card-container">
           <h2>Todo</h2>
-          {/*@todo - rating select component */}
           <div className="btn-container">
-            <button className="btn btn-primary" type="submit">
+            <button className="btn btn-primary" onClick={handleCreateReminder}>
               Create Reminder
             </button>
           </div>
